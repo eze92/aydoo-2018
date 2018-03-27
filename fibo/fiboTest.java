fd01c4ae6ca7a6b8b02a5531d0b32421ffdd8163
Ezequiel <ezequiel.ledesma026@gmail.com>
2018-03-26 23:08:25 -0300

    Ejercicio Fibonacci

--- fibo/fiboTest.java
+++ fibo/fiboTest.java
@@ -2,44 +2,45 @@ package fibo;
 
 import junit.framework.Assert;
 
-import static org.junit.Assert.assertArrayEquals;
-
 import org.junit.Test;
 
 public class fiboTest {
 
+	//Tests de la funcion fibonacci
+	
 	@Test
 	public void prueba() {
 		fibo fi = new fibo();
-        int esperado = 55;
-        int real= fi.fibonacci(10);
+        	int esperado = 55;
+        	int real= fi.fibonacci(10);
         Assert.assertEquals(esperado, real);
         
 	  }
 	@Test
 	public void prueba2() {
 		fibo fi = new fibo();
-        int esperado = 1;
-        int real= fi.fibonacci(1);
+        	int esperado = 1;
+        	int real= fi.fibonacci(1);
         Assert.assertEquals(esperado, real);
 	}
 	@Test
 	public void prueba3() {
 		fibo fi = new fibo();
-        int esperado = 5;
-        int real= fi.fibonacci(5);
+       	 	int esperado = 5;
+        	int real= fi.fibonacci(5);
         Assert.assertEquals(esperado, real);
  }
 	@Test
 	public void prueba4() {
 		fibo fi = new fibo();
-		Assert.assertEquals(55,fi.fibonacci(10));
+	Assert.assertEquals(55,fi.fibonacci(10));
 	}
 	@Test
 	public void prueba5() {
 		fibo fi = new fibo();
-		Assert.assertEquals(-1,fi.fibonacci(-1));
+	Assert.assertEquals(-1,fi.fibonacci(-1));
 	}
 
 }
+
 
